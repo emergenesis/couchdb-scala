0917aaedbf3f7c5409366c225542b2dcb8cc41f9
Josh D Miller <josh@joshdmiller.com>
2011-10-30 11:12:22 -0700

    Added ScalaTest for Specs (BDD)

---
+++ src/test/scala/org/gestaltfoundation/couchdb/ConnectionTest.scala
@@ -0,0 +1,18 @@
+package org.gestaltfoundation.couchdb
+
+import org.scalatest.Spec
+
+class ConnectionTest extends Spec {
+    
+    describe ( "A Connection" ) {
+        it ( "should successfully connect to the specified CouchDB instance" ) {
+            pending
+        }
+
+        it ( "should have the appropriate connection metaddata" ) {
+            pending
+        }
+    }
+}
+
+// vim: set ts=4 sw=4 et:
